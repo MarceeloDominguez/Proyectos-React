9be3c7344e68df6d1dc6ffb5da81bbafeeb0fbbc
marcelo dominguez <marceelodominguez@gmail.com>
2023-06-20 18:41:47 -0300

    responsive navbar

--- 04-food-shopping/src/components/Drawer.tsx
+++ 04-food-shopping/src/components/Drawer.tsx
@@ -16,10 +16,10 @@ export default function Drawer() {
     <div className="bg-slate-400/50 z-50 fixed right-0 top-0 h-full w-full">
       <div
         //style={{ scrollbarWidth: "thin" }}
-        className="bg-[#F9F8F8] z-50 fixed right-0 top-0 h-full lg:w-[30%] md:w-[65%] w-full rounded-tl-[20px] rounded-bl-[20px] transition-transform duration-1000 ease-in-out overflow-y-auto"
+        className="bg-[#F9F8F8] z-50 fixed right-0 top-0 h-full lg:w-[30%] md:w-[65%] w-full md:rounded-tl-[20px] rounded-bl-[20px] transition-transform duration-1000 ease-in-out overflow-y-auto"
       >
         {/* <style>{`::-webkit-scrollbar { display: none;}`}</style> */}
-        <div className="fixed top-0 right-0 lg:w-[30%] md:w-[65%] w-full h-14 p-4 rounded-tl-[20px] bg-[#F9F8F8]">
+        <div className="fixed top-0 right-0 lg:w-[30%] md:w-[65%] w-full h-14 p-4 md:rounded-tl-[20px] bg-[#F9F8F8]">
           <button onClick={() => closeDrawer()}>
             <IoClose size={25} />
           </button>
@@ -34,7 +34,7 @@ export default function Drawer() {
             ))}
           </div>
         </div>
-        <div className="bg-[#F9F8F8] border-t-2 border-[#DEDDDD] fixed bottom-0 right-0 lg:w-[30%] md:w-[65%] w-full h-28 rounded-bl-[20px]">
+        <div className="bg-[#F9F8F8] border-t-2 border-[#DEDDDD] fixed bottom-0 right-0 lg:w-[30%] md:w-[65%] w-full h-28 md:rounded-bl-[20px]">
           <div className="pt-2 px-6">
             <div className="flex items-center justify-between mb-4">
               <p className="text-textPrimary font-bold md:text-lg">Shipping</p>
--- 04-food-shopping/src/components/NavbarResponsive.tsx
+++ 04-food-shopping/src/components/NavbarResponsive.tsx
@@ -5,10 +5,19 @@ import { AiOutlineMenu } from "react-icons/ai";
 import { RxCross1 } from "react-icons/rx";
 import { LuShoppingCart } from "react-icons/lu";
 import { useContextGlobalCart } from "@/context/GlobalContext";
+import Link from "next/link";
+import { useCartStore } from "@/store/cartStore";
+
+const links = [
+  { label: "Home", route: "/" },
+  { label: "About", route: "/about" },
+  { label: "Shop", route: "/shop" },
+];
 
 export default function NavbarResponsive() {
   const [showMenu, setShowMenu] = useState(false);
   const { openDrawer } = useContextGlobalCart();
+  const { productsCart } = useCartStore();
 
   return (
     <nav className="lg:hidden flex justify-between items-center container mx-auto px-5">
@@ -16,17 +25,38 @@ export default function NavbarResponsive() {
       <Logo width={100} height={54} />
       <button
         onClick={() => openDrawer()}
-        className="bg-textPrimary h-8 w-8 rounded-full flex items-center justify-center"
+        className="bg-textPrimary h-8 w-8 rounded-full flex items-center justify-center relative"
       >
         <LuShoppingCart size={16} color="#fff" />
+        <div className="bg-orange-600 absolute -top-2 left-5 h-6 w-6 rounded-full flex items-center justify-center">
+          <span className="text-black font-bold text-[12px]">
+            {productsCart.length}
+          </span>
+        </div>
       </button>
       <div
-        className={`fixed left-0 top-0 bg-red-500 ${
+        className={`fixed left-0 top-0 bg-[#F9F8F8] ${
           showMenu ? "w-[70%] h-full" : "w-0 h-0 opacity-0"
         }`}
       >
         <div className="absolute right-10 top-5">
           <RxCross1 size={22} onClick={() => setShowMenu(false)} />
+        </div>
+        <div className="mt-20">
+          <div className="flex justify-center mb-7 py-2">
+            <Logo width={198} height={54} />
+          </div>
+          <ul className="flex flex-col gap-8">
+            {links.map((item, index) => (
+              <Link
+                href={item.route}
+                key={index}
+                className="text-textPrimary text-xl font-semibold px-4"
+              >
+                {item.label}
+              </Link>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
