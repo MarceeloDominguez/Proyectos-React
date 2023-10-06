0d295f4b0872fb4dab8de799ed50fb34f5e351d3
marcelo dominguez <marceelodominguez@gmail.com>
2023-10-06 17:21:06 -0300

    add baseurl

--- 06-bazar-universal/client/src/app/cart/page.tsx
+++ 06-bazar-universal/client/src/app/cart/page.tsx
@@ -1,9 +1,10 @@
+import Footer from "@/components/Footer";
 import BackPage from "@/components/cart/BackPage";
 import ListProducts from "@/components/cart/ListProducts";
 
 export default function Cart() {
   return (
-    <div className="min-h-screen bg-slate-100">
+    <div className="min-h-screen flex flex-col justify-between bg-slate-100">
       <div className="lg:w-3/5 md:w-3/4 container mx-auto md:px-6 px-3 pt-8">
         <BackPage />
         <p className="text-slate-900 font-bold text-center md:text-left text-xl py-4">
@@ -11,6 +12,7 @@ export default function Cart() {
         </p>
         <ListProducts />
       </div>
+      <Footer />
     </div>
   );
 }
--- 06-bazar-universal/client/src/components/cart/PriceTotal.tsx
+++ 06-bazar-universal/client/src/components/cart/PriceTotal.tsx
@@ -20,9 +20,9 @@ export default function PriceTotal() {
   return (
     <div className="mt-10">
       <p className="text-lg text-slate-900 font-bold">
-        Precio final: <span>${priceTotal}</span>
+        Precio Final: <span>${priceTotal}</span>
       </p>
-      <div className="flex mt-4">
+      <div className="flex py-4">
         <button className="bg-[#53B175] md:w-[50%] w-full flex items-center justify-center h-10 rounded-xl text-slate-100 font-bold text-sm">
           Comprar ahora
         </button>
